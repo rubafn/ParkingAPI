281da2cc54a5fd94e6e8a7796f3d5bae876f22e0
Ruba <rubanabhan123@gmail.com>
2026-08-10 11:41:45 +0300

    added repositories

---
+++ parking/src/main/java/com/example/parking/Repository/ReservationRepository.java
@@ -0,0 +1,8 @@
+package com.example.parking.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.parking.model.Reservation;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Integer>{
+}
---
+++ parking/src/main/java/com/example/parking/Repository/VehicleRepository.java
@@ -0,0 +1,8 @@
+package com.example.parking.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.parking.model.Vehicle;
+
+public interface VehicleRepository extends JpaRepository<Vehicle, String>{
+}
