2aff7c29ec8de269e42063d9c58b2083e3853a00
Ruba <rubanabhan123@gmail.com>
2026-08-11 14:54:52 +0300

    added exceptions and handled them

--- parking/src/main/java/com/example/parking/Exceptions/GlobalHandler.java
+++ parking/src/main/java/com/example/parking/Exceptions/GlobalHandler.java
@@ -12,4 +12,16 @@ public class GlobalHandler {
     public ResponseEntity<String> handleVehicleNotFound(NoVehicleFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
+    @ExceptionHandler(NoTicketFoundException.class)
+    public ResponseEntity<String> handleTicketNotFound(NoTicketFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+    @ExceptionHandler(NoAvailableSpotsException.class)
+    public ResponseEntity<String> handleNoAvailableSpots(NoAvailableSpotsException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+    @ExceptionHandler(AlreadyParkedException.class)
+    public ResponseEntity<String> handleAlreadyParked(AlreadyParkedException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
 }
