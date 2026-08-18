4cabdaa130614a7883ebf6d213d1299bc8bde3e1
Ruba <rubanabhan123@gmail.com>
2026-08-18 11:46:32 +0300

    added spot number min/max value constraint

--- parking/src/main/java/com/example/parking/DTO/SpotAddRequest.java
+++ parking/src/main/java/com/example/parking/DTO/SpotAddRequest.java
@@ -2,9 +2,13 @@ package com.example.parking.DTO;
 
 import com.example.parking.VehicleType;
 
-public class SpotAddRequest {
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 
+public class SpotAddRequest {
     private int branchId;
+    @Min(1)
+    @Max(999)
     private int spotNumber;
     private VehicleType type;
 
--- parking/src/main/java/com/example/parking/DTO/SpotUpdateRequest.java
+++ parking/src/main/java/com/example/parking/DTO/SpotUpdateRequest.java
@@ -2,9 +2,14 @@ package com.example.parking.DTO;
 
 import com.example.parking.VehicleType;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
 public class SpotUpdateRequest {
     private VehicleType type;
     private Boolean isAvailable;
+    @Min(1)
+    @Max(999)
     private int spotNumber;
 
     public VehicleType getType() {
